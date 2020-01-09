38abba2d9c4aca71fddf732881dfd48861abec4c
LeoRocha <leorochasilva27@gmail.com>
2020-01-09 15:23:21 -0300

    Primeiro commit, subindo apenas a interface do projeto

---
+++ BibliotecaOraculo/FormHome.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BibliotecaOraculo
+{
+    partial class FormHome
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelSuperior = new System.Windows.Forms.Panel();
+            this.panelBotoes = new System.Windows.Forms.Panel();
+            this.buttonSair = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTipBarraSuperior = new System.Windows.Forms.ToolTip(this.components);
+            this.panelBotoes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSuperior
+            // 
+            this.panelSuperior.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSuperior.Location = new System.Drawing.Point(0, 0);
+            this.panelSuperior.Name = "panelSuperior";
+            this.panelSuperior.Size = new System.Drawing.Size(1008, 18);
+            this.panelSuperior.TabIndex = 0;
+            this.panelSuperior.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelSuperior_MouseDown);
+            this.panelSuperior.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelSuperior_MouseMove);
+            this.panelSuperior.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelSuperior_MouseUp);
+            // 
+            // panelBotoes
+            // 
+            this.panelBotoes.BackColor = System.Drawing.SystemColors.Menu;
+            this.panelBotoes.Controls.Add(this.buttonSair);
+            this.panelBotoes.Controls.Add(this.button3);
+            this.panelBotoes.Controls.Add(this.button2);
+            this.panelBotoes.Controls.Add(this.button1);
+            this.panelBotoes.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelBotoes.Location = new System.Drawing.Point(0, 18);
+            this.panelBotoes.Name = "panelBotoes";
+            this.panelBotoes.Size = new System.Drawing.Size(186, 583);
+            this.panelBotoes.TabIndex = 1;
+            // 
+            // buttonSair
+            // 
+            this.buttonSair.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.buttonSair.FlatAppearance.BorderSize = 0;
+            this.buttonSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSair.Font = new System.Drawing.Font("Segoe WP", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSair.ForeColor = System.Drawing.Color.White;
+            this.buttonSair.Location = new System.Drawing.Point(3, 523);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(179, 57);
+            this.buttonSair.TabIndex = 3;
+            this.buttonSair.Text = "Sair";
+            this.buttonSair.UseVisualStyleBackColor = false;
+            this.buttonSair.Click += new System.EventHandler(this.buttonSair_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 601);
+            this.Controls.Add(this.panelBotoes);
+            this.Controls.Add(this.panelSuperior);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            this.panelBotoes.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSuperior;
+        private System.Windows.Forms.Panel panelBotoes;
+        private System.Windows.Forms.ToolTip toolTipBarraSuperior;
+        private System.Windows.Forms.Button buttonSair;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BibliotecaOraculo/FormHome.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BibliotecaOraculo
+{
+    public partial class FormHome : Form
+    {
+        bool mouseDown;
+        private Point offset;
+        public FormHome()
+        {
+            InitializeComponent();
+
+            toolTipBarraSuperior.SetToolTip(panelSuperior, "Clique e arraste para mover a tela");
+        }
+
+
+
+//Eventos Para Mexer a Tela
+        private void panelSuperior_MouseDown(object sender, MouseEventArgs e)
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+            mouseDown = true;
+        }
+        private void panelSuperior_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown == true)
+            {
+                Point currentScreenPos = PointToScreen(e.Location);
+                Location = new Point(currentScreenPos.X - offset.X, currentScreenPos.Y - offset.Y);
+            }
+        }
+        private void panelSuperior_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void buttonSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
